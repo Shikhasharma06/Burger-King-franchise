9b1cd0172b281d2421aab90828b7147ea98947d9
Shikha sharma <135316685+Shikhasharma06@users.noreply.github.com>
2023-11-18 22:26:37 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+export default {
+	content: ['*.html'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				menu: 'url("../assets/menu.jpg")',
+			},
+		},
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: ['light'],
+	},
+}
